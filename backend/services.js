746eebae1ef5db6c239c3bb296da40009349142b
Devashish Priyadarshi <devashish234073@gmail.com>
2022-03-28 09:13:25 +0530

    added code for the backend

--- backend/services.js
+++ backend/services.js
@@ -1,1 +1,24 @@
-var express = require('express');
+
+const express = require('express')
+const { exec } = require("child_process");
+const app = express()
+const port = 3000
+
+app.get('/run', (req, res) => {
+  var cmnd = req.query.cmnd;
+  exec(cmnd, (error, stdout, stderr) => {
+    if (error) {
+        res.end(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        res.end(`stderr: ${stderr}`);
+        return;
+    }
+    res.end(`${stdout}`);
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
